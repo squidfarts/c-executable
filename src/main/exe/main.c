0f028262d062b2af6e4b7e303783f39e12f41e78
Michael Brockus <squidfarts@icloud.com>
2019-09-08 20:43:59 -0700

    First commit.

---
+++ src/main/exe/main.c
@@ -0,0 +1,29 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.c                                                                    *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: Apache 2.0 :http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                                                 *
+ ***********************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, Meson' is displayed to the standard console.
+ *
+ */
+int main(void)
+{
+    puts("Hello, Meson.");
+    
+    return EXIT_SUCCESS;
+}// end of function main
